ec42e7c9ab23a664087f3d4e353501e4d5756a0d
Nuwan Kodagoda <nuwank@gmail.com>
2023-05-08 03:52:56 +0530

    Initial Commit

---
+++ TickTackToeDemo/TickTackToeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTackToeDemoApp.swift
+//  TickTackToeDemo
+//
+//  Created by Nuwan Kodagoda on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TickTackToeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
